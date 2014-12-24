2369e2b1fe2dafa7eb264aed5bc806550428e28f
bstaykov <boycho.staykov@gmail.com>
2014-12-24 11:50:18 +0200

    missing model

---
+++ Web/BlogSystem.Web/Models/ManageUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogSystem.Web.Models
+{
+    public class ManageUserViewModel
+    {
+        public string NewPassword { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string OldPassword { get; set; }
+    }
+}
