7ea7f60203c7be09a79b18c3bc04445e3592d980
bstaykov <boycho.staykov@gmail.com>
2015-04-07 13:20:45 +0300

    Tag re-added

---
+++ Models/BlogSystem.Models/Tag.cs
@@ -0,0 +1,30 @@
+﻿namespace BlogSystem.Models
+{
+    using System.Collections.Generic;
+
+    public class Tag
+    {
+        private ICollection<Post> posts;
+
+        public Tag()
+        {
+            this.posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public virtual ICollection<Post> Posts
+        {
+            get
+            {
+                return this.posts;
+            }
+            set
+            {
+                this.posts = value;
+            }
+        }
+    }
+}
