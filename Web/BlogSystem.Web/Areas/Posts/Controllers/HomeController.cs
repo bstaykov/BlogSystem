66ea34c548c7c3039a57567a9009f3a9037c1baa
bstaykov <boycho.staykov@gmail.com>
2015-04-09 15:17:24 +0300

    BaseController, HomeConroller added

---
+++ Web/BlogSystem.Web/Areas/Posts/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿namespace BlogSystem.Web.Areas.Posts.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Microsoft.AspNet.Identity;
+
+    using BlogSystem.Common.Repository;
+    using BlogSystem.Models;
+    using BlogSystem.Web.Areas.Posts.Models;
+    using BlogSystem.Web.Infrastructure.Filters;
+
+    public class HomeController : Controller
+    {
+        private readonly IRepository<Post> posts;
+
+        public HomeController(IRepository<Post> posts)
+        {
+            this.posts = posts;
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 1)]
+        public ActionResult Index()
+        {
+            var allPosts = this.posts.All()
+                .Select(PostViewModel.FromPost)
+                .OrderBy(post => post.DateTimePosted)
+                .ToList();
+
+            return View(allPosts);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult InsertPost()
+        {
+            return View(new PostInputModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult InsertPost(PostInputModel post)
+        {
+            if (ModelState.IsValid)
+            {
+                this.TempData["success"] = "Post was added!";
+
+                Post newPost = new Post()
+                {
+                    Title = post.Title,
+                    Content = post.Content,
+                    DateTimePosted = DateTime.Now,
+                    UserId = User.Identity.GetUserId(),
+                    CommentsCount = 0,
+                    Likes = 0
+                };
+
+                this.posts.Add(newPost);
+                this.posts.SaveChanges();
+
+                return RedirectToAction("InsertPost");
+            }
+
+            return this.View(post);
+        }
+
+        [HttpGet]
+        [Authorize]
+        [OutputCache(Duration = 1)]
+        public ActionResult MyPosts()
+        {
+            string userId = User.Identity.GetUserId();
+
+            var myPosts = this.posts.All()
+                .AsQueryable()
+                .Where(post => post.UserId == userId)
+                .OrderBy(post => post.DateTimePosted)
+                .Select(MyPostViewModel.FromPost)
+                .ToList();
+
+            return View(myPosts);
+        }
+    }
+}
---
+++ Web/BlogSystem.Web/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogSystem.Web.Controllers
+{
+    using BlogSystem.Web.Infrastructure.Filters;
+    using System.Web.Mvc;
+
+    [Log]
+    public abstract class BaseController : Controller
+    {
+        // GET: Base
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
